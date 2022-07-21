afc4401e04de259106b8ac4b9121f6096e595049
TitanV <hojoon.dev@gmail.com>
2022-07-22 04:25:18 +0900

    update - log

--- src/pm2.ts
+++ src/pm2.ts
@@ -7,7 +7,7 @@ export const getProgramList = async () => {
   await pm2.connect(async (err) => {
     await pm2.list(async (err, list) => {
       await list.forEach(async (data) => {
-        console.log('[check] - Check pm2 program name:', data);
+        console.log('[check] - Check pm2 program name:', data.name);
         if (data.name && !process.env['EXCLUDE_PM2_PROCESS_NAME_LIST']?.split(',').includes(data.name)) {
           // 이름 정합성 테스트 후에 해야 함
           console.log('[check] - none exist');
