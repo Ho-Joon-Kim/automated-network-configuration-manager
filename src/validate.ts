08bc05e00eb66d48b9915db6638f034034d9429f
TitanV <hojoon.dev@gmail.com>
2022-07-22 13:35:04 +0900

    fix - typo

--- src/validate.ts
+++ src/validate.ts
@@ -1,8 +1,8 @@
 export const isProgramPortValidated = (programName: string) => {
   try {
     const [name, port] = programName.split(':');
-    if (5000 <= Number(port) && 5300 > Number(port)) return false;
-    return true;
+    if (5000 <= Number(port) && 5300 > Number(port)) return true;
+    return false;
   } catch (error) {
     return false;
   }
